3f8e585cf9e8fb5385d693e931b52c465cc2c59e
unknown <1469406888@mil>
2019-03-08 13:54:00 -0800

    edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ console.log(computerChoice);
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = '';
+var lettersGuessed = [];
 
 var winsTxt = document.getElementById("wins-txt");
 var lossesTxt = document.getElementById("losses-txt");
@@ -15,31 +15,34 @@ var guessesSoFarTxt = document.getElementById("guesses-so-far-txt");
 document.addEventListener('keyup', userInput);
 function userInput(event) {
     var userGuess = event.key.toLowerCase();
-    var newGuess;
-
-    if (userGuess === computerChoice) {
-        wins++;
-        winsTxt.textContent = "Wins: " + wins;
-        resetGuesses();
-
-	// did not implement check if userGuess already exists so same key will continue to count down
-    } else if (userGuess != computerChoice && guessesLeft > 1) {
-        guessesLeft--;
-        guessesLeftTxt.textContent = "Guesses left: " + guessesLeft;
-        newGuess = document.createElement("span");
-        newGuess.textContent = userGuess + " ";
-        guessesSoFarTxt.appendChild(newGuess);
-    } else if (userGuess != computerChoice && guessesLeft === 1) {
-        losses++;
-        lossesTxt.textContent = "Losses: " + losses;
-        resetGuesses();
-    } 
+	var key = event.keyCode;
+	
+	if (key >= 65 && key <= 90 && lettersGuessed.indexOf(userGuess) === -1) {
+		lettersGuessed.push(userGuess);
+		if (userGuess === computerChoice) {
+				wins++;
+				winsTxt.textContent = "Wins: " + wins;
+				resetGuesses();
+			} else if (userGuess != computerChoice && guessesLeft > 1) {
+				guessesLeft--;
+				guessesLeftTxt.textContent = "Guesses left: " + guessesLeft;
+				guessesSoFarTxt.textContent = "Your guesses so far: " + lettersGuessed.join(" ");
+			} else if (userGuess != computerChoice && guessesLeft === 1) {
+				losses++;
+				lossesTxt.textContent = "Losses: " + losses;
+				resetGuesses();
+			} 
+	}
+		
 }
 
 function resetGuesses() {
     guessesLeft = 9;
+	lettersGuessed = [];
+
     guessesLeftTxt.textContent = "Guesses left: " + guessesLeft;
     guessesSoFarTxt.textContent = "Your guesses so far: ";
+
     computerChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
     console.log(computerChoice);
 }
